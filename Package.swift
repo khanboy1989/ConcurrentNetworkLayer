6531f1667c6a6b81761b630410bee745229365af
Serhan Khan <serhankhan0@gmail.com>
2025-03-11 11:05:53 +0200

    first commit

--- Package.swift
+++ Package.swift
@@ -3,8 +3,15 @@
 
 import PackageDescription
 
+let settings: [SwiftSetting] = [
+  .enableExperimentalFeature("StrictConcurrency")
+]
+
 let package = Package(
     name: "ConcurrentNetworkManager",
+    platforms: [
+        .iOS(.v17)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -15,7 +22,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ConcurrentNetworkManager"),
+            name: "ConcurrentNetworkManager",
+            swiftSettings: settings
+        ),
         .testTarget(
             name: "ConcurrentNetworkManagerTests",
             dependencies: ["ConcurrentNetworkManager"]
--- Sources/ConcurrentNetworkManager/ConcurrentNetworkManager.swift
+++ Sources/ConcurrentNetworkManager/ConcurrentNetworkManager.swift
@@ -1,2 +1,19 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+open class UserInfo: @unchecked Sendable {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+
+actor SwiftDataRaceManager {
+    var userInfo: UserInfo = .init(name: "")
+    func updateDatabase(_ userInfo: UserInfo) async throws {
+        self.userInfo = userInfo
+    }
+}
+
